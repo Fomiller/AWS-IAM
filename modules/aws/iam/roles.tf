4ee99cc136c9567600ac1c9f56e3184185c91dfc
Forrest Miller <forrestmillerj@gmail.com>
2022-12-30 13:01:16 -0600

    fix

--- modules/aws/iam/roles.tf
+++ modules/aws/iam/roles.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "test_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_policy.json
 }
 
-resource "aws_iam_role" "congocoon_lambda_role" {
+resource "aws_iam_role" "hello_world_lambda_role" {
     name               = "LambdaHelloWorld"
     assume_role_policy = data.aws_iam_policy_document.lambda_assume_policy.json
 }
@@ -14,6 +14,6 @@ resource "aws_iam_role_policy_attachment" "test_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "hello_world_policy_attachment" {
-    role = aws_iam_role.test_role.name
+    role = aws_iam_role.hello_world_lambda_role.name
     policy_arn = aws_iam_policy.hello_world_lambda_role_policy.arn
 }
