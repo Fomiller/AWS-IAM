fe574ebc5af68eb58801f39d4f36b5648326a775
Forrest Miller <forrestmillerj@gmail.com>
2023-01-02 13:47:02 -0600

    removed test role/policies

--- modules/aws/iam/roles.tf
+++ modules/aws/iam/roles.tf
@@ -3,11 +3,6 @@ resource "aws_iam_role" "hello_world_lambda_role" {
     assume_role_policy = data.aws_iam_policy_document.lambda_assume_policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "test_policy" {
-    role = aws_iam_role.test_role.name
-    policy_arn = aws_iam_policy.test_policy.arn
-}
-
 resource "aws_iam_role_policy_attachment" "hello_world_policy_attachment" {
     role = aws_iam_role.hello_world_lambda_role.name
     policy_arn = aws_iam_policy.hello_world_lambda_role_policy.arn
