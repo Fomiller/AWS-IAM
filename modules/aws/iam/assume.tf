953e7a28e6ee1e9d47c1f0a198b31f84c795211a
Forrest Miller <forrestmillerj@gmail.com>
2023-02-19 16:43:21 -0600

    eventbridge ecs role

--- modules/aws/iam/assume.tf
+++ modules/aws/iam/assume.tf
@@ -1,11 +1,23 @@
 data "aws_iam_policy_document" "lambda_assume_policy" {
   statement {
-    sid     = "STSassumeRole"
+    sid     = "LambdaAssumePolicy"
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "eventbridge_assume_policy" {
+  statement {
+    sid     = "EventBridgeAssumePolicy"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
     }
   }
 }
--- modules/aws/iam/roles.tf
+++ modules/aws/iam/roles.tf
@@ -17,3 +17,13 @@ resource "aws_iam_role_policy_attachment" "aws_terraform" {
   role       = aws_iam_role.aws_terraform.name
   policy_arn = data.aws_iam_policy.admin_access.arn
 }
+
+resource "aws_iam_role" "eventbridge_ecs_role" {
+  name               = "CsEventBridgeEcsRole"
+  assume_role_policy = data.aws_iam_policy_document.eventbridge_assume_policy.json
+}
+
+# resource "aws_iam_role_policy_attachment" "ecs_events" {
+#   role       = aws_iam_role.aws_terraform.name
+#   policy_arn = data.aws_iam_policy.admin_access.arn
+# }
