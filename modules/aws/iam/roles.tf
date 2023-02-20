1d04784172045bb99e0f376bdf05c180ac67b7e6
Forrest Miller <forrestmillerj@gmail.com>
2023-02-19 18:56:48 -0600

    added max session duration for deployer role

--- modules/aws/iam/roles.tf
+++ modules/aws/iam/roles.tf
@@ -9,8 +9,9 @@ resource "aws_iam_role_policy_attachment" "hello_world_policy_attachment" {
 }
 
 resource "aws_iam_role" "aws_terraform" {
-  name               = "AWSTERRAFORM"
-  assume_role_policy = data.aws_iam_policy_document.aws_terraform_assume_policy.json
+  name                 = "AWSTERRAFORM"
+  assume_role_policy   = data.aws_iam_policy_document.aws_terraform_assume_policy.json
+  max_session_duration = 43200
 }
 
 resource "aws_iam_role_policy_attachment" "aws_terraform" {
