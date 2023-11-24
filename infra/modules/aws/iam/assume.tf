190d683169b5bf5b104a60f67871652335c904c2
Forrest Miller <forrestmillerj@gmail.com>
2023-11-24 16:07:18 -0600

    removed roles

--- infra/modules/aws/iam/assume.tf
+++ infra/modules/aws/iam/assume.tf
@@ -1,27 +1,3 @@
-data "aws_iam_policy_document" "lambda_assume_policy" {
-  statement {
-    sid     = "LambdaAssumePolicy"
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "eventbridge_assume_policy" {
-  statement {
-    sid     = "EventBridgeAssumePolicy"
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-  }
-}
-
 data "aws_iam_policy_document" "aws_terraform_assume_policy" {
   statement {
     sid    = "STSassumeRole"
--- infra/modules/aws/iam/roles.tf
+++ infra/modules/aws/iam/roles.tf
@@ -1,13 +1,3 @@
-resource "aws_iam_role" "hello_world_lambda_role" {
-  name               = "LambdaHelloWorld"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "hello_world_policy_attachment" {
-  role       = aws_iam_role.hello_world_lambda_role.name
-  policy_arn = aws_iam_policy.hello_world_lambda_role_policy.arn
-}
-
 resource "aws_iam_role" "aws_terraform" {
   name                 = "AWSTERRAFORM"
   assume_role_policy   = data.aws_iam_policy_document.aws_terraform_assume_policy.json
@@ -17,14 +7,4 @@ resource "aws_iam_role" "aws_terraform" {
 resource "aws_iam_role_policy_attachment" "aws_terraform" {
   role       = aws_iam_role.aws_terraform.name
   policy_arn = data.aws_iam_policy.admin_access.arn
-}
-
-resource "aws_iam_role" "eventbridge_ecs_role" {
-  name               = "CsEventBridgeEcsRole"
-  assume_role_policy = data.aws_iam_policy_document.eventbridge_assume_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_events" {
-  role       = aws_iam_role.eventbridge_ecs_role.name
-  policy_arn = aws_iam_policy.ecs_events_run_task_with_any_role.arn
 }
