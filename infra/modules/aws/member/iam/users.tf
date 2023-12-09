d6e34f873c5f67e88239cf39bd89d61b6a351835
Forrest Miller <forrestmillerj@gmail.com>
2023-12-08 21:33:23 -0600

    data resource

--- infra/modules/aws/member/iam/users.tf
+++ infra/modules/aws/member/iam/users.tf
@@ -5,6 +5,6 @@ resource "aws_iam_user" "terraform_user" {
 }
 
 resource "aws_iam_user_policy_attachment" "terraform_role" {
-  user       = aws_iam_user.admin.name
+  user       = data.aws_iam_user.admin.name
   policy_arn = aws_iam_policy.cross_account_route53.arn
 }
