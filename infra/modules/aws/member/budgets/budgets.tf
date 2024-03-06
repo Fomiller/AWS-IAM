25c2ea38c215f19e68ebbf25385d9797d49641f3
Forrest Miller <forrestmillerj@gmail.com>
2024-03-05 20:41:18 -0600

    added time_unit to budget

--- infra/modules/aws/member/budgets/budgets.tf
+++ infra/modules/aws/member/budgets/budgets.tf
@@ -2,6 +2,7 @@ resource "aws_budgets_budget" "cost" {
   budget_type  = "COST"
   limit_amount = "200"
   limit_unit   = "USD"
+  time_unit    = "MONTHLY"
 
   notification {
     comparison_operator        = "GREATER_THAN"
